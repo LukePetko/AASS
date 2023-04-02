7d0af5e85c9786f55c923842c35ebe44632b2150
Lukáš Peťko <luke.petko@gmail.com>
2023-04-02 23:43:34 +0200

    adding new screens

--- apps/frontend/src/routes/contentRouter.tsx
+++ apps/frontend/src/routes/contentRouter.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "../screens/Dashboard";
+import NewHoliday from "../screens/NewHoliday";
+import Review from "../screens/Review";
 
 const router = createBrowserRouter([
   {
-    path: "/register",
+    path: "/new-holiday",
+    element: <NewHoliday />,
   },
+  { path: "/review", element: <Review /> },
   {
     path: "/",
     element: <Dashboard />,
---
+++ apps/frontend/src/screens/NewHoliday.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewHoliday = () => {
+  return (
+    <div>
+      <h1>New Holiday</h1>
+    </div>
+  );
+};
+
+export default NewHoliday;
